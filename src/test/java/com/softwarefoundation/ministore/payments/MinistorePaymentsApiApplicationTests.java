f0d34ee3b3ebbb1fad675be6018e08129d7bbe9c
Dherkyan Ribeiro da Silva <dherkyan@gmail.com>
2021-05-05 00:13:30 -0400

    Inicio projeto ministore-payments-api

---
+++ src/test/java/com/softwarefoundation/ministore/payments/MinistorePaymentsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwarefoundation.ministore.payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinistorePaymentsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
